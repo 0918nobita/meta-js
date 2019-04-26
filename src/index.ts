136c36b54e8e524d56adec55195d0c7a349d22e1
0918nobita <nobita.0918@gmail.com>
2019-04-26 19:52:32 +0900

    :sparkles: 算術演算子をすべて * に変換するサンプルを追加した

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { PluginObj } from "babel-core";
+
+const { transform } = require("@babel/core");
+
+const src = '1 + 2 / 3';
+
+const plugin = ({ types }: { types: any }): PluginObj => ({
+  visitor: {
+    BinaryExpression(nodePath) {
+      if (nodePath.node.operator !== '*') {
+        const newAst = types.binaryExpression('*', nodePath.node.left, nodePath.node.right);
+        nodePath.replaceWith(newAst);
+      }
+    }
+  }
+});
+
+const { code } = transform(src, { plugins: [plugin] });
+console.log(code);
